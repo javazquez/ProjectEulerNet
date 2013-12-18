155c0771692045bd63ee643ec9ee42f7661a558b
Juan Vazquez <juanvazquez@gmail.com>
2013-12-17 22:09:42 -0600

    Create 002.exs
    adding elixir solution to 002

---
+++ 002.exs
@@ -0,0 +1,9 @@
+;;; Juan Vazquez 
+;;; https://github.com/javazquez
+;;; http://javazquez.com
+
+Stream.unfold({0,1}, fn {f1,f2} -> {f1, {f2, f1+f2}} end) |>
+Stream.filter(fn x -> Integer.even?x end) |> 
+Enum.take_while(fn y -> y < 4000000 end ) |> 
+Enum.reduce(fn x,acc-> x+acc end) |> 
+IO.puts
