5ee6f24e24305075be6a62e3145c8d28a9b0ccb4
Juan Vazquez <juanvazquez@gmail.com>
2013-12-17 22:08:15 -0600

    Update 001.exs
    adding header

--- 001.exs
+++ 001.exs
@@ -1,3 +1,7 @@
+# Juan Vazquez 
+# https://github.com/javazquez
+# http://javazquez.com
+
 m = Enum.reduce Enum.filter(1..999,&( rem( &1,3) == 0 || rem( &1,5) == 0 )), fn(val,acc)-> val+acc end
 IO.puts "#{m}"
 
