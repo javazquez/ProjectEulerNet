138bca654604e5838a3dbf703b5e4e7fc8da5d44
Juan Vazquez <juanvazquez@gmail.com>
2013-12-17 22:10:08 -0600

    Update 002.exs
    fixing comments

--- 002.exs
+++ 002.exs
@@ -1,6 +1,6 @@
-;;; Juan Vazquez 
-;;; https://github.com/javazquez
-;;; http://javazquez.com
+# Juan Vazquez 
+# https://github.com/javazquez
+# http://javazquez.com
 
 Stream.unfold({0,1}, fn {f1,f2} -> {f1, {f2, f1+f2}} end) |>
 Stream.filter(fn x -> Integer.even?x end) |> 
