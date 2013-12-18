d5c01c0b5712f928e540172882630ddb04407471
Juan Vazquez <juanvazquez@gmail.com>
2013-12-17 22:07:14 -0600

    Create 001.exs
    elixir solution to 001

---
+++ 001.exs
@@ -0,0 +1,4 @@
+m = Enum.reduce Enum.filter(1..999,&( rem( &1,3) == 0 || rem( &1,5) == 0 )), fn(val,acc)-> val+acc end
+IO.puts "#{m}"
+
+#m = Enum.reduce Enum.filter(1..999,&( rem( &1,3) == 0 || rem( &1,5) == 0 )), &(&1+&2)
