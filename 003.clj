26e95c91056106ea60a3f9bc83ec967576c99d3a
Juan Vazquez <juanvazquez@gmail.com>
2014-01-08 10:53:42 -0600

    format code

--- 003.clj
+++ 003.clj
@@ -6,12 +6,12 @@
   (< (count (filter #(zero? (rem num %))(range 1 num ))) 3))
     
 (defn factors [num]
-	(filter #(zero? (rem num %)) (range 1 num) ))
+  (filter #(zero? (rem num %)) (range 1 num)))
 	
 (defn largest-prime-factor[num]
- (->> (factors num)
-  (take-while #(and (<= (* %1 2 ) num) (prime? %1)))
-	(reduce max)))
+  (->> (factors num)
+    (take-while #(and (<= (* %1 2 ) num) (prime? %1)))
+    (reduce max)))
 
 ;user=> (largest-prime-factor 600851475143)
 ;6857
