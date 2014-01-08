486a6a6bf2000d2f12dd65d833ad38c16cda3d70
Juan Vazquez <juanvazquez@gmail.com>
2014-01-08 11:30:09 -0600

    Create 004.exs

---
+++ 004.exs
@@ -0,0 +1,14 @@
+# Juan Vazquez 
+# https://github.com/javazquez
+# http://javazquez.com
+
+defmodule Euler do
+	def palindroms(vals) do
+	  lc x inlist vals,
+		 y inlist vals, to_string(x*y) === String.reverse(to_string(x*y)) do
+	   x*y
+		end	
+	end
+end
+
+Euler.palindroms(Enum.to_list(100..999)) |> Enum.max |> IO.puts
