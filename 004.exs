28b75fcfe356ca6b023d0125e6e08a2b040a0982
Juan Vazquez <juanvazquez@gmail.com>
2014-01-08 11:31:13 -0600

    Update 004.exs

--- 004.exs
+++ 004.exs
@@ -3,12 +3,12 @@
 # http://javazquez.com
 
 defmodule Euler do
-	def palindroms(vals) do
-	  lc x inlist vals,
-		 y inlist vals, to_string(x*y) === String.reverse(to_string(x*y)) do
-	   x*y
-		end	
-	end
+  def palindroms(vals) do
+    lc x inlist vals,
+      y inlist vals, to_string(x*y) === String.reverse(to_string(x*y)) do
+      x*y
+    end	
+  end
 end
 
 Euler.palindroms(Enum.to_list(100..999)) |> Enum.max |> IO.puts
