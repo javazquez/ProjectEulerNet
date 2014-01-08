0694dd3c0220c691c9a28d5511ca158a5fc54a49
Juan Vazquez <juanvazquez@gmail.com>
2014-01-08 11:31:51 -0600

    format code

--- 003.exs
+++ 003.exs
@@ -3,14 +3,14 @@
 # http://javazquez.com 
 
 defmodule Euler3 do
-	defp prime?(num) do
-		Stream.filter( 1..num, &( 0 === (rem(num , &1)))) |> 
-		Enum.to_list |> 
-		Enum.count < 3
-	end	
-	defp factors(num), do: Stream.filter(1..num, &( 0 === (rem(num , &1))))
-	def primeFactors(num), do: factors(num) |>
-	 Stream.take_while(&(num >= &1*2 && prime?(&1)))	
+  defp prime?(num) do
+    Stream.filter( 1..num, &( 0 === (rem(num , &1)))) |> 
+    Enum.to_list |> 
+    Enum.count < 3
+  end	
+  defp factors(num), do: Stream.filter(1..num, &( 0 === (rem(num , &1))))
+  def primeFactors(num), do: factors(num) |>
+    Stream.take_while(&(num >= &1*2 && prime?(&1)))	
 end
 
 Euler3.primeFactors(600851475143) |> 
