d802bf915b1e261071625ab6dcaeb11d004cc1d4
Juan Vazquez <juanvazquez@gmail.com>
2014-01-09 14:13:52 -0600

    Update 005.exs

--- 005.exs
+++ 005.exs
@@ -3,8 +3,8 @@
 # http://javazquez.com
 
 defmodule Euler do 
-		defp gcd(a,b), do: (if b==0, do: a, else: gcd(b, rem(a, b)))
-		def lcm(a, b), do: div(a * b, gcd(a,b))
+  defp gcd(a,b), do: (if b==0, do: a, else: gcd(b, rem(a, b)))
+  def lcm(a, b), do: div(a * b, gcd(a,b))
 end
 
 IO.puts Enum.reduce(1..20, fn x,y -> Euler.lcm(x,y) end) 
