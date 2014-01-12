eb9e88235658ee9f19a269ed97163faa2c41a2f1
Juan Vazquez <juanvazquez@gmail.com>
2014-01-12 09:34:56 -0600

    format code

--- 007.exs
+++ 007.exs
@@ -5,18 +5,18 @@
 # http://projecteuler.net/problem=7
 
 defmodule Euler do
-	defp nextPrime(canidate,[h|t], cnt//3) when cnt <= 10001 do
-		if Enum.all?([h|t], &(rem(canidate,&1) !== 0 )) do
-			nextPrime(canidate+2, [canidate|[h|t]], cnt+1)
-		else
-			nextPrime(canidate + 2, [h|t], cnt)
-		end
-	end
-	defp nextPrime(_,[h|t], _), do: [h|t]
-	def solve do
-		nextPrime(5,[3,2]) |> hd
+  defp nextPrime(canidate,[h|t], cnt//3) when cnt <= 10001 do
+    if Enum.all?([h|t], &(rem(canidate,&1) !== 0 )) do
+      nextPrime(canidate+2, [canidate|[h|t]], cnt+1)
+    else
+      nextPrime(canidate + 2, [h|t], cnt)
+    end
+  end
+  defp nextPrime(_,[h|t], _), do: [h|t]
+  def solve do
+    nextPrime(5,[3,2]) |> hd
   end
 end
 
- Euler.solve |> IO.puts
- #104743
+Euler.solve |> IO.puts
+#104743
