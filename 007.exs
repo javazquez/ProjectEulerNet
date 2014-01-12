52e1445a5c297ed235b4313b9fc02d30f3e3ee8a
Juan Vazquez <juanvazquez@gmail.com>
2014-01-12 09:17:08 -0600

    Update 007.exs

--- 007.exs
+++ 007.exs
@@ -5,22 +5,22 @@
 # http://projecteuler.net/problem=7
 
 defmodule Euler do
-	def nextPrime(canidate,[h|t]) do
-		if Enum.all?([h|t], &(rem(canidate,&1) !== 0 )) do
-			[canidate|[h|t]] #prime so return it
-		else
-			nextPrime(canidate + 2, [h|t])
-		end
-	end
-	def getNthPrime(x//10000) do #generator
-		Stream.iterate([3], &(nextPrime(hd(&1)+2,&1))) |>
-		Stream.take(x) |> 
-		Enum.reverse |> 
-		hd |> #[nth prime|[list of n primes]]
-		hd #nth prime
-	end
+  def nextPrime(canidate,[h|t]) do
+    if Enum.all?([h|t], &(rem(canidate,&1) !== 0 )) do
+      [canidate|[h|t]] #prime so return it
+    else
+      nextPrime(canidate + 2, [h|t])
+    end
+  end
+  def getNthPrime(x//10000) do #generator
+    Stream.iterate([3], &(nextPrime(hd(&1)+2,&1))) |>
+    Stream.take(x) |> 
+    Enum.reverse |> 
+    hd |> #[nth prime|[list of n primes]]
+    hd #nth prime
+  end
 end
 
- Euler.getNthPrime |> 
- IO.puts
- #104743
+  Euler.getNthPrime |> 
+  IO.puts
+  #104743
