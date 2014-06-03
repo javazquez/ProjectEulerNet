37ada502453375b878b1729cfa6d0f13350a90e7
Juan Vazquez <juanvazquez@gmail.com>
2014-06-03 18:54:42 -0500

    Create 001.js

---
+++ 001.js
@@ -0,0 +1,12 @@
+// Juan Vazquez
+// https://github.com/javazquez
+// http://javazquez.com
+// Clojure Solution to Project Euler problem 1
+// http://projecteuler.net/problem=1
+var sum = 0;
+for(var i=1; i< 1000; i++){
+  if(i % 3 === 0 || i % 5 === 0 ){
+    sum += i;
+  }
+}
+console.log(sum); //233168
