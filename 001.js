5892b96d5439b6e343b1fbe1a6c942bc89575ebb
Juan Vazquez <juanvazquez@gmail.com>
2014-06-03 21:36:20 -0500

    correcting copy and paste header error

--- 001.js
+++ 001.js
@@ -1,7 +1,7 @@
 // Juan Vazquez
 // https://github.com/javazquez
 // http://javazquez.com
-// Clojure Solution to Project Euler problem 1
+// Javascript Solution to Project Euler problem 1
 // http://projecteuler.net/problem=1
 var sum = 0;
 for(var i=1; i< 1000; i++){
