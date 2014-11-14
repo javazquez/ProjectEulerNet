676fd494df353a71a5e920186429836af259022e
Juan Vazquez <juanvazquez@gmail.com>
2014-11-14 10:08:02 -0600

    Update 006.exs

--- 006.exs
+++ 006.exs
@@ -3,6 +3,7 @@
 # http://javazquez.com
 # Elixir Solution to Project Euler problem 6
 # http://projecteuler.net/problem=6
+# Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
 defmodule Euler do
 	def solve(x), do: (x * x - 1) * (3*x + 2) * x/12
