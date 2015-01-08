8223567bb5f9dee4467c4f3e8a3c19cd74031b9a
Juan Vazquez <juanvazquez@gmail.com>
2015-01-08 14:44:15 -0600

    Update 001.hs
    added comment

--- 001.hs
+++ 001.hs
@@ -5,6 +5,7 @@
  Haskell Solution to Project Euler problem 1
  http://projecteuler.net/problem=1
  Find the sum of all the multiples of 3 or 5 below 1000.
+ NOTE: used interactive ghci prompt to test
 -}
 sum [x| x <- [3..999], x `mod` 3 == 0 || x `mod`5 == 0  ] 
 --233168
