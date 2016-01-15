d1153b3e71128e8369a2df29fceab4af07f5873f
Juan Vazquez <juanvazquez@gmail.com>
2016-01-15 08:50:21 -0600

    fix the ordering

--- 007.clj
+++ 007.clj
@@ -4,6 +4,12 @@
 ;; Clojure Solution to Project Euler problem 7
 ;; http://projecteuler.net/problem=7
 
+(defn prime? [ candidate plist]
+  (->> plist
+    (map #(rem candidate %) )
+    (some zero?)
+    not))
+
 (defn solve [ nth ]
   (loop [ candidate 3
           plist '(3 2 )
@@ -14,12 +20,6 @@
       (if (prime? candidate plist)
         (recur (+ 2 candidate) (conj plist candidate) (inc acc))
         (recur (+ 2 candidate) plist   acc)))))
-
-(defn prime? [ candidate plist]
-  (->> plist
-    (map #(rem candidate %) )
-    (some zero?)
-    not))
 
 (time (solve 10001))
 ;"Elapsed time: 52900.907781 msecs"
